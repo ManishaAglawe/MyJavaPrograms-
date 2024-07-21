fa8a630abe9e8fb9a25bf69edb7f45882e31f2cb
manisha <aglawe.manisha@gmail.com>
2024-07-21 13:48:53 +0530

    Array

---
+++ Five_Names_Array.java
@@ -0,0 +1,21 @@
+package Array_Prog;
+
+import java.util.Arrays;
+
+public class Five_Names_Array {
+
+	public static void main(String[] args) 
+	{
+
+		String name[]=new String [5];
+		name[0]="manisha";
+		name[1]="sushma";
+		name[2]="akash";
+		name[3]="Mahesh";
+		name[4]="Mom";
+		System.out.println(Arrays.toString(name));
+		
+
+	}
+
+}

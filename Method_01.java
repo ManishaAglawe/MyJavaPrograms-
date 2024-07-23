6fb135b53ed3af7c53a9f1458a2452f845d31a6a
manisha <aglawe.manisha@gmail.com>
2024-07-23 12:43:29 +0530

    access specifier

---
+++ Method_01.java
@@ -0,0 +1,18 @@
+package Access_Specifier_Package001_Have_Relation_withotherclass;
+
+import Access_Specifier_Package002_Have_Relation_withotherclass.Method_02;
+
+public class Method_01 extends Method_02
+{
+    
+	public static void main(String[] args)
+	{
+		Method_01 m1=new Method_01();
+		m1.add(); // public and protected only be access 
+		m1.sub();
+		
+		
+
+	}
+
+}

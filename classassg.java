9a9bddb2f2555cdc4208f39d0d802f37f780eea5
manisha <aglawe.manisha@gmail.com>
2024-07-24 10:22:41 +0530

    basic programs

---
+++ classassg.java
@@ -0,0 +1,20 @@
+package BasicPrograms;
+
+public class classassg {
+
+	public static void main(String[] args) 
+	{
+/*	int []s1= {3,6,7,2,4};
+	
+	
+	for(int sum=0; sum<s1[].length;sum++)
+	{
+		if(s1[i]%2)==0)
+         {
+	       System.out.println("");
+          }
+	}
+     
+	}*/
+
+}

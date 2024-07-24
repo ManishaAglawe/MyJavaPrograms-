72f6dec274699c2564022f48dd0f42455487f048
manisha <aglawe.manisha@gmail.com>
2024-07-24 10:04:01 +0530

    object concept

---
+++ Class1_Object.java
@@ -0,0 +1,11 @@
+package Object_Concept_Java;
+
+public class Class1_Object 
+{
+	public static void main(String[] args) 
+	{
+		Class1_Object c1=new Class1_Object();
+		System.out.println(c1.getClass());
+	}
+
+}

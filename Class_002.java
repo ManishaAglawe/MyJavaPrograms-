44b6443566ebcc0ef41d4b67778d9885054a499b
manisha <aglawe.manisha@gmail.com>
2024-07-24 10:25:50 +0530

    access specifier

---
+++ Class_002.java
@@ -0,0 +1,23 @@
+package Access_Specifier_Package2_Not_Have_Relation_withotherclass;
+
+public class Class_002 
+{
+	public void add()
+	{
+		System.out.println("add");
+	}
+	protected void sub()
+	{
+		System.out.println("sub");
+	}
+	private void mul()
+	{
+		System.out.println("mul");
+	}
+	void div()
+	{
+		System.out.println("div");
+		
+	}
+
+}

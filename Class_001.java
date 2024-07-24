7d784d63283ec0c5ac9055bbde5c65c014d100fc
manisha <aglawe.manisha@gmail.com>
2024-07-24 10:51:33 +0530

    access specifier scope

---
+++ Class_001.java
@@ -0,0 +1,14 @@
+package Access_Specifier_Package1_Not_Have_Relation_withotherclass;
+
+import Access_Specifier_Package2_Not_Have_Relation_withotherclass.Class_002;
+
+public class Class_001 {
+
+	public static void main(String[] args) {
+		Class_002 c1=new Class_002();
+		c1.add();//This is an public method so only public method we can access in another package not private,default,protected 
+		
+
+	}
+
+}
---
+++ Method_02.java
@@ -0,0 +1,23 @@
+package Access_Specifier_Package002_Have_Relation_withotherclass;
+
+public class Method_02
+{
+	public void add()
+	{
+		System.out.println("add");
+	}
+	protected void sub()
+	{
+		System.out.println("sub");
+	}
+	private void mul()
+	{
+		System.out.println("mul");
+	}
+	void div()
+	{
+		System.out.println("div");
+		
+	}
+
+}
